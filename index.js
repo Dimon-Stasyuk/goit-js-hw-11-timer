905f3847ef1c55d68a6b32122f04821d954f5e30
Dimon-Stasyuk <78039077+Dimon-Stasyuk@users.noreply.github.com>
2021-07-31 18:50:28 +0300

    исправил ошибки

--- index.js
+++ index.js
@@ -1,50 +1,52 @@
-const refs = {
-    days: document.querySelector('[data-value="days"]'),
-    hours: document.querySelector('[data-value="hours"]'),
-    mins: document.querySelector('[data-value="mins"]'),
-    secs: document.querySelector('[data-value="secs"]'),
+class CountdownTimer {
+  constructor({ targetDate, selector }) {
+    this.selector = selector;
+    this.targetDate = targetDate;
+
+    this.days = document.querySelector(`${this.selector} [data-value="days"]`);
+    this.hours = document.querySelector(
+      `${this.selector} [data-value="hours"]`
+    );
+    this.mins = document.querySelector(`${this.selector} [data-value="mins"]`);
+    this.secs = document.querySelector(`${this.selector} [data-value="secs"]`);
+  }
+  start() {
+    const TimerTime = Date.now() - this.targetDate;
+    const currentTime = new Date();
+    setInterval(() => {
+      const time = new Date() - currentTime;
+      const deltaTime = TimerTime - time;
+      const { days, hours, mins, secs } = this.getTimeComponents(deltaTime + 6);
+
+      this.days.textContent = days;
+      this.hours.textContent = hours;
+      this.mins.textContent = mins;
+      this.secs.textContent = secs;
+    }, 1000);
+  }
+  getTimeComponents(time) {
+    const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+    const hours = pad(
+      Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+    );
+    const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
+    return { days, hours, mins, secs };
+  }
 }
 
-
-class CountdownTimer {
-    constructor({ targetDate, selector }) {
-        this.selector = selector;
-       this.targetDate = targetDate;
-   }
-    start() {
-        const TimerTime = Date.now() - this.targetDate;
-        const currentTime = new Date();
-        setInterval(() => {
-            
-        const time = new Date() - currentTime;
-        const deltaTime = TimerTime - time;
-        const {days, hours, mins, secs} = getTimeComponents(deltaTime + 6)
-            refs.days.textContent = days;
-            refs.hours.textContent = hours;
-            refs.mins.textContent = mins;
-            refs.secs.textContent = secs;            
-    }, 1000);
-    };
-};
-
-
-
 const timer = new CountdownTimer({
-  selector: '#timer-1',
-  targetDate: new Date('Jul 26, 2019'),
+  selector: "#timer-1",
+  targetDate: new Date("Jul 26, 2019"),
+});
+
+const timer2 = new CountdownTimer({
+  selector: "#timer-2",
+  targetDate: new Date("Jul 26, 2021"),
 });
 
 timer.start();
 
-
-function getTimeComponents(time) {
-const days =pad(Math.floor(time / (1000 * 60 * 60 * 24))) ;
-const hours =pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))) ;
-const mins =pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60))) ;
-    const secs =pad(Math.floor((time % (1000 * 60)) / 1000)) ;
-    return {days, hours, mins, secs}
-}
-
 function pad(value) {
-    return String(value).padStart(2, '0');
+  return String(value).padStart(2, "0");
 }
